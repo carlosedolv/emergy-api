cf50640e0801293c4550caa3710de6ad1389a984
Carlos <carlosmcof@gmail.com>
2026-02-01 13:31:06 -0300

    feat: initial project structure

---
+++ src/main/java/com/carlosedolv/emergy_api/EmergyApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlosedolv.emergy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosedolv/emergy_api/EmergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosedolv.emergy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
